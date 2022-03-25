a71b573b6c739ae69426cd7a31a64f3f96236dbb
Arnas Rimkus <arnas.rimkus@yahoo.com>
2022-03-25 18:09:11 +0200

    PS-8 fixed husky hooks location error

--- dbilo-siuntos-frontend/src/tests/components/DateDisplay.test.tsx
+++ dbilo-siuntos-frontend/src/tests/components/DateDisplay.test.tsx
@@ -3,7 +3,8 @@ import { render, screen } from '@testing-library/react';
 import { DateDisplay } from '../../components/DateDisplay';
 
 test('renders current date', () => {
+  //Random test, don't mind me
   render(<DateDisplay />);
   const timeFormat = screen.getByText(/GMT/i);
-  expect(timeFormat).toBeInTheDocument();
+  expect(timeFormat).not.toBeInTheDocument();
 });
