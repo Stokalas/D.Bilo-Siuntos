6713442b27e243f4f83304b1a8a07ccabf3254cb
unknown <matas.lazdauskas@gmail.com>
2022-03-28 17:31:45 +0300

    PS-10 initial entity framework setup

---
+++ Infrastructure/Models/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    internal class DatabaseContext : DbContext
+    {
+        public DbSet<Parcel> Parcels { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base (options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                var connectionString = "Server=(localdb)\\MSSQLLocalDB; Database=LibraryDb; Integrated Security=True;";
+
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
