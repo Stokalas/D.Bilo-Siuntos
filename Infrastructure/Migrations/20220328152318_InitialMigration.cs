eb6344f348b4182674cc5f71455de6bd4b734bcb
unknown <matas.lazdauskas@gmail.com>
2022-03-28 18:25:50 +0300

    PS-10 initial migration

---
+++ Infrastructure/Migrations/20220328152318_InitialMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parcels",
+                columns: table => new
+                {
+                    TrackingNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parcels", x => x.TrackingNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parcels");
+        }
+    }
+}
