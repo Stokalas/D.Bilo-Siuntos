58fc0e993123012f25d2e6c0138eba10de14edc0
MatasLa <66332915+MatasLa@users.noreply.github.com>
2022-03-28 21:01:08 +0300

    Feature/ps 10 setup database (#4)
    * PS-10 initial entity framework setup
    
    * PS-10 default connection settings
    
    * PS-10 initial migration
    
    * PS-10 fixed warnings
    
    * PS-10 added encrypt=true to connection string

---
+++ Infrastructure/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base (options) { }
+        public DbSet<Parcel>? Parcels { get; set; }
+    }
+}
