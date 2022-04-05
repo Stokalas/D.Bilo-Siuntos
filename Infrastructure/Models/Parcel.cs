593ef1042d4762ff4ae62bc8ef2a56390772a17a
unknown <matas.lazdauskas@gmail.com>
2022-04-05 16:11:06 +0300

    PS-31 updated parcel model

--- Infrastructure/Models/Parcel.cs
+++ Infrastructure/Models/Parcel.cs
@@ -7,11 +7,19 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Models
 {
+    public enum Size
+    {
+        S, M, L, XL
+    }
     public class Parcel
     {
         public int Id { get; set; }
         [Key]
         public string? TrackingNumber { get; set; }
-        
+        public DateTime DeliveryDateTime { get; set; }
+        public string? DeliveryAddress { get; set; }
+        public Size? Size { get; set; }
+
+
     }
 }
