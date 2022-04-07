784e7fd89931e8cbb7d52aee63f9e028f8fa2ed4
Gytis Becalis <gytis.becalis@gmail.com>
2022-04-07 10:30:49 +0300

    ps-19 added mock get endpoint

---
+++ WebAPI/Controllers/ParcelController.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    public class ParcelController : Controller
+    {
+        private readonly ILogger<ParcelController> _logger;
+        public ParcelController(ILogger<ParcelController> logger)
+        {
+            _logger = logger;
+        }
+
+        /*
+         * Only a mock, I think there's no DB infrastructure yet, and Parcel properties will change...
+         * We shouldn't use EF here :)
+        */
+        [HttpGet("parcel/all")]
+        public ActionResult<IEnumerable<Parcel>> Get()
+        {
+            _logger.LogInformation("Received GET request at parcel/all"); //testing purposes
+            return new[]
+            {
+                new Parcel { Id = 1, TrackingNumber = Guid.NewGuid().ToString()},
+                new Parcel { Id = 2, TrackingNumber = Guid.NewGuid().ToString()},
+                new Parcel { Id = 3, TrackingNumber = Guid.NewGuid().ToString()}
+            };
+        }
+    }
+}
