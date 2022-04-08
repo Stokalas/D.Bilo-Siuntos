1311c30f5ee399c9e091995178c6ac91b794113e
unknown <matas.lazdauskas@gmail.com>
2022-04-08 18:27:50 +0300

    PS-31 update User model

--- Infrastructure/Models/User.cs
+++ Infrastructure/Models/User.cs
@@ -9,12 +9,12 @@ namespace Infrastructure.Models
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public Address Address { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public Address? Address { get; set; }
+        public string? PhoneNumber { get; set; }
 
-        public ICollection<Parcel> Parcels { get; set; }
+        public ICollection<Parcel>? Parcels { get; set; }
     }
 }
