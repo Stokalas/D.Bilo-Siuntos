263b77ba44e2c0d9c54e0958954925a73bf6e2a7
Arnas Rimkus <arnas.rimkus@yahoo.com>
2022-04-10 12:34:57 +0300

    PS-37 delete mock tests

--- dbilo-siuntos-frontend/src/tests/pages/HomePage.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import { HomePage } from '../../pages/HomePage';
-
-test('renders hello world message', () => {
-  render(<HomePage />);
-  const greetings = screen.getByText(/Hello world/i);
-  expect(greetings).toBeInTheDocument();
-});
