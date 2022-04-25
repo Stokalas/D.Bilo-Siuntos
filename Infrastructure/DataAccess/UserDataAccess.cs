20b9dbf6c1025f07f2dab633c159da1b88264435
unknown <matas.lazdauskas@gmail.com>
2022-04-25 18:34:06 +0300

    Data access for user

---
+++ Infrastructure/DataAccess/UserDataAccess.cs
@@ -0,0 +1,107 @@
+﻿using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DataAccess
+{
+    public class UserDataAccess
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public UserDataAccess(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User> GetById(int id)
+        {
+            try
+            {
+                var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == id);
+                if (user == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                return user;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<IList<User>> GetAll()
+        {
+            try
+            {
+                return await _dbContext.Users.ToListAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task Insert(User newUser)
+        {
+            try
+            {
+                await _dbContext.Users.AddAsync(newUser);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            try
+            {
+                var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == id);
+                if (user == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                _dbContext.Users.Remove(user);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task Update(int id, User updatedUser)
+        {
+            try
+            {
+                var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == id);
+                if (user == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                user.Name = updatedUser.Name;
+                user.Email = updatedUser.Email;
+                user.Address = updatedUser.Address;
+                user.LastName = updatedUser.LastName;
+                user.PhoneNumber = updatedUser.PhoneNumber;
+                user.Parcels = updatedUser.Parcels;
+
+                _dbContext.Users.Update(user);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
