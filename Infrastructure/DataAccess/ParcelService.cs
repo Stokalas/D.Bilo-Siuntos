f0f3faceae27d6ea1d65704ffaca03d0dd05f47c
unknown <matas.lazdauskas@gmail.com>
2022-04-25 19:27:11 +0300

    PS-45 added ParcelService

---
+++ Infrastructure/DataAccess/ParcelService.cs
@@ -0,0 +1,114 @@
+﻿using Infrastructure.Interfaces;
+using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DataAccess
+{
+    public class ParcelService : IParcelService
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public ParcelService(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Parcel> GetByTrackingId(string trackingId)
+        {
+            try
+            {
+                var parcel = await _dbContext.Parcels.FirstOrDefaultAsync(x => x.TrackingNumber == trackingId);
+                if (parcel == null)
+                {
+                    return null;
+                }
+                return parcel;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<IList<Parcel>> GetAllByUserId(int id)
+        {
+            try
+            {
+                return await _dbContext.Parcels.Where(x => x.ShipperID == id).ToListAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<IList<Parcel>> GetAll()
+        {
+            try
+            {
+                return await _dbContext.Parcels.ToListAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task Insert(Parcel newParcel)
+        {
+            try
+            {
+                await _dbContext.Parcels.AddAsync(newParcel);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<Parcel> Delete(string trackingId)
+        {
+            try
+            {
+                var parcel = await _dbContext.Parcels.FirstOrDefaultAsync(x => x.TrackingNumber == trackingId);
+                if (parcel == null)
+                {
+                    return null;
+                }
+                _dbContext.Parcels.Remove(parcel);
+                await _dbContext.SaveChangesAsync();
+                return parcel;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<Parcel> Update(string trackingId, Parcel updatedParcel)
+        {
+            try
+            {
+                var parcel = await _dbContext.Parcels.FirstOrDefaultAsync(x => x.TrackingNumber == trackingId);
+                if (parcel == null)
+                {
+                    return null;
+                }
+                parcel.ShipmentDate = updatedParcel.ShipmentDate;
+                parcel.ShippingAddress = updatedParcel.ShippingAddress;
+
+                parcel.DeliveryDate = updatedParcel.DeliveryDate;
+                parcel.DeliveryAddress = updatedParcel.DeliveryAddress;
+
+                _dbContext.Parcels.Update(parcel);
+                await _dbContext.SaveChangesAsync();
+                return parcel;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
