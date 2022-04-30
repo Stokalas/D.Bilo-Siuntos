339eff303e65fbe6905c318bc2f35cb3ef87b845
MatasLa <66332915+MatasLa@users.noreply.github.com>
2022-04-30 07:14:09 +0300

    Feature/ps 45 create services for db interaction (#19)
    * Data access for user
    
    * added IUser interface
    
    * PS-45 updated names of classes, updated UserService methods
    
    * PS-45 updated name of class
    
    * PS-45 added ParcelService
    
    * PS-45 updated UserService, removed spaces
    
    * PS-45 renamed folder
    
    * PS-45 fixed mistake with wrong class name, added returns to methods where it was missing, added exception print
    
    * PS-45 update for try/catch

---
+++ Infrastructure/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<User> GetById(int id);
+        public Task<IList<User>> GetAll();
+        public Task<User> Insert(User newUser);
+        public Task<User> Delete(int id);
+        public Task<User> Update(int id, User updatedUser);
+    }
+}
