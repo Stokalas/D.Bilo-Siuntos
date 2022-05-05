642ea18abfa15cc4540e2fa54e77cf365f07aaa7
unknown <matas.lazdauskas@gmail.com>
2022-05-05 16:23:23 +0300

    PS-59 create parcel model

---
+++ Infrastructure/Migrations/20220505122106_ParcelStatusMigration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class ParcelStatusMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Status",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParcelStatus = table.Column<int>(type: "int", nullable: false),
+                    ParcelTrackingNumber = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Status", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Status_Parcels_ParcelTrackingNumber",
+                        column: x => x.ParcelTrackingNumber,
+                        principalTable: "Parcels",
+                        principalColumn: "TrackingNumber");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Status_ParcelTrackingNumber",
+                table: "Status",
+                column: "ParcelTrackingNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Status");
+        }
+    }
+}
---
+++ Infrastructure/Models/Status.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Enums;
+
+namespace Infrastructure.Models
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public ParcelStatus ParcelStatus { get; set; }
+        public Parcel Parcel { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
