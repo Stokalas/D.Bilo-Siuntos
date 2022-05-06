2a11f8c873ac60a215ca5e8c75293d88df0a32a0
MatasLa <66332915+MatasLa@users.noreply.github.com>
2022-05-06 16:31:23 +0300

    bugfix updated address and parcel models, created new migration (#24)
    * bugfix updated address and parcel models, created new migration
    
    * removed duplications from User entity
    
    * bugfix updated address entity

---
+++ Infrastructure/Migrations/20220506132750_UpdatedParcelMigration.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class UpdatedParcelMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Parcels_Address_DeliveryAddressId",
+                table: "Parcels");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ParcelId",
+                table: "Status",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ShipperID",
+                table: "Parcels",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DeliveryAddressId",
+                table: "Parcels",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Address",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Address",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Address",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Address",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Address",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Address",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Address",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Parcels_Address_DeliveryAddressId",
+                table: "Parcels",
+                column: "DeliveryAddressId",
+                principalTable: "Address",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Parcels_Address_DeliveryAddressId",
+                table: "Parcels");
+
+            migrationBuilder.DropColumn(
+                name: "ParcelId",
+                table: "Status");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Address");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ShipperID",
+                table: "Parcels",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DeliveryAddressId",
+                table: "Parcels",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Address",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Parcels_Address_DeliveryAddressId",
+                table: "Parcels",
+                column: "DeliveryAddressId",
+                principalTable: "Address",
+                principalColumn: "Id");
+        }
+    }
+}
--- Infrastructure/Models/Parcel.cs
+++ Infrastructure/Models/Parcel.cs
@@ -10,12 +10,12 @@ namespace Infrastructure.Models
         public string TrackingNumber { get; set; }
         public ParcelSize Size { get; set; }
 
-        public int ShipperID { get; set; }
+        public int? ShipperID { get; set; }
         public DateTime ShipmentDate { get; set; }
         public Address? ShippingAddress { get; set; }
 
         public DateTime? DeliveryDate { get; set; }
-        public Address? DeliveryAddress { get; set; }
+        public Address DeliveryAddress { get; set; }
 
         public ICollection<Status> Status { get; set; }
     }
