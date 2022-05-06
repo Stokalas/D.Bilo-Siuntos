3beff0d75e2b7be78180a269e519863f80f9815e
Arnas Rimkus <arnas.rimkus@yahoo.com>
2022-05-06 14:52:30 +0300

    PS-56 removed comment

--- dbilo-siuntos-frontend/src/types/address.ts
+++ dbilo-siuntos-frontend/src/types/address.ts
@@ -6,9 +6,3 @@ export interface Address {
   postalCode: string;
   country: string;
 }
-// public int Id { get; set; }
-// public string City { get; set; }
-// public string AddressLine1 { get; set; }
-// public string? AddressLine2 { get; set; }
-// public string PostalCode { get; set; }
-// public string Country { get; set; }
