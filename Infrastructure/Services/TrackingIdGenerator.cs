58563d02424bf0522130904ba4548617debfa3ac
Deividas Kučinskas <deividask.lt@gmail.com>
2022-05-08 00:21:50 +0300

    ps 69 Created tracking number generator and updated POST and GET requests accordingly

---
+++ Infrastructure/Services/TrackingIdGenerator.cs
@@ -0,0 +1,12 @@
+﻿namespace Infrastructure.Services
+{
+    public class TrackingIdGenerator
+    {
+        public string GenerateId()
+        {
+            Guid g = Guid.NewGuid();
+            var trackingId = g.ToString().Remove(0, 24);
+            return trackingId;
+        }
+    }
+}
