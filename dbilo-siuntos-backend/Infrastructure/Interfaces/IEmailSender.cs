9a53546550d669d9fa2d284804aa847b2464ad84
Yar3k <djelement567@gmail.com>
2022-05-27 17:36:23 +0300

    add user logging

--- dbilo-siuntos-backend/Infrastructure/Interfaces/IEmailSender.cs
+++ dbilo-siuntos-backend/Infrastructure/Interfaces/IEmailSender.cs
@@ -4,7 +4,7 @@ namespace Infrastructure.Interfaces
 {
     public interface IEmailSender
     {
-        public void SendSender(RecipientDetails data, string tracking);
-        public void SendReceiver(RecipientDetails data, Address address, string tracking);
+        public void SendSender(RecipientDetails data, string tracking, string username);
+        public void SendReceiver(RecipientDetails data, Address address, string tracking, string username);
     }
 }
